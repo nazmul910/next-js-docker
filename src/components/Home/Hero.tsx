b2a65eb4b1848ca3ac6efb73a12a2cc70372b427
nazmul910 <islamnasir910@gmail.com>
2026-06-06 20:47:21 +0600

    Refactor Home component structure and add Hero component

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = () =>{
     return(
         <>
-            <div>
+            <div className="bg-black h-screen w-full flex items-center justify-center flex-col gap-4">
                 <h1>Welcome to the Home Page</h1>
                 <p>This is a simple home page component.</p>
             </div>
---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "../components/Home/Hero";
+
+export default function HomePage() {
+    return(
+        <>
+            <div className="bg-black h-auto w-full">
+                <Hero/>
+            </div>
+        </>
+    )
+}
