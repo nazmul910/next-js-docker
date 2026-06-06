2a11c68fc5e099e5c732bca9d0f713af5c8d8ae7
nazmul910 <islamnasir910@gmail.com>
2026-06-06 20:26:24 +0600

    Add Hero and Home components for the main page

---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,10 @@
+export const Hero = () =>{
+    return(
+        <>
+            <div>
+                <h1>Welcome to the Home Page</h1>
+                <p>This is a simple home page component.</p>
+            </div>
+        </>
+    )
+}  
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>Welcome to the Home Page</h1>
+                <p>This is a simple home page component.</p>
+            </div>
+        </>
+    )
+}
