64d84e0d1553d3495d3cadf40a72bfa7b5f66207
nazmul910 <islamnasir910@gmail.com>
2026-06-10 21:24:01 +0600

    fix: update global ignores in ESLint config for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,10 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
+   "**/.next/**",
+    "**/out/**",
+    "**/build/**",
+    "**/node_modules/**",
     "next-env.d.ts",
   ]),
 ]);
