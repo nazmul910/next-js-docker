7b3eafce1580872fc98974902afb27c42c57ae4b
nazmul910 <islamnasir910@gmail.com>
2026-06-10 23:53:05 +0600

    changing heading

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () =>{
         <>
             <div className="bg-black h-screen w-full flex items-center justify-center flex-col gap-4">
                 <div>
-                    <h1>Docker Setup Project</h1>
+                    <h1>Docker Setup Projects</h1>
                     <p className="text-gray-400">A project to set up Docker for development and deployment.</p>
                 </div>
 
