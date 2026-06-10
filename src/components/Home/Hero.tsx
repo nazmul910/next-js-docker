2f72343d494b0fb75814ef8333118b5d5245d2b3
nazmul910 <islamnasir910@gmail.com>
2026-06-10 21:05:13 +0600

    feat: add Docker image to Hero component and adjust layout

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import DockerImage from "../../../assets/hero-home/docker.webp"
+
 export const Hero = () =>{
     return(
         <>
-            <div className="bg-black h-screen w-full flex items-center justify-center flex-col gap-4">
+            <div className="bg-black h-auto w-full flex items-center justify-center flex-col gap-4">
                 <h1>Docker Setup Project</h1>
                 <p className="text-gray-400">A project to set up Docker for development and deployment.</p>
+
+                <div className="w-full flex items-center justify-center mt-8">
+                    <div className="w-1/2 flex items-center justify-center border border-white rounded-3xl relative overflow-hidden">
+                        <Image src={DockerImage} alt="Docker Image" width={400} height={400} />
+                    </div>
+
+                </div>
             </div>
         </>
     )
